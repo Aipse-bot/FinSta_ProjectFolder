2063ef081092d73ab368909976c02140dce125fa
Kent Espia <kentvirgil.espia@cit.edu>
2025-04-12 21:55:15 +0800

    Added gameuser to store the game progress

--- module1/admin.py
+++ module1/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import GameUser
 # Register your models here.
+
+admin.site.register(GameUser)
+# This code registers the GameUser model with the Django admin site, allowing it to be managed through the admin interface.
--- module1/models.py
+++ module1/models.py
@@ -1,3 +1,30 @@
+from django.contrib.auth.models import AbstractUser, Group, Permission
 from django.db import models
 
-# Create your models here.
+class GameUser(AbstractUser):
+    option_1 = models.IntegerField(default=0)
+    option_2 = models.IntegerField(default=0)
+    option_3 = models.IntegerField(default=0)
+
+    groups = models.ManyToManyField(
+        Group,
+        verbose_name=('groups'),
+        blank=True,
+        help_text=(
+            'The groups this user belongs to. A user will get all permissions '
+            'granted to each of their groups.'
+        ),
+        related_name="gameuser_set",  # Add this line
+        related_query_name="gameuser", # Add this line
+    )
+    user_permissions = models.ManyToManyField(
+        Permission,
+        verbose_name=('user permissions'),
+        blank=True,
+        help_text=('Specific permissions for this user.'),
+        related_name="gameuser_permissions_set", # Add this line
+        related_query_name="gameuser_permission", # Add this line
+    )
+
+    def __str__(self):
+        return self.username
