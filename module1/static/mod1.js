a88df4a967c5f180f69788dd92977ff48e8462d8
Rienel <renBasil1016@gmail.com>
2025-04-13 23:08:00 +0800

    Finished 4-6 TODO

--- module1/static/mod1.js
+++ module1/static/mod1.js
@@ -185,3 +185,52 @@ function openModal(optId){
 function redirectToPage(link){
     window.location.href = link
 }
+
+// todo 4-6
+document.addEventListener('DOMContentLoaded', function() {
+    // todo 4.) After clicking the colored boxes it should redirect to 'mod1t4.html'
+    const goalOptions = document.querySelectorAll('#goal-opt1, #goal-opt2, #goal-opt3');
+    
+    goalOptions.forEach(option => {
+        option.addEventListener('click', function() {
+            window.location.href = '/mod1t4';
+        });
+        option.style.cursor = 'pointer';
+    });
+
+    const nameOptionElements = document.querySelectorAll('.goal-icon');
+    const businessNameInput = document.querySelector('.businessNameInput');
+    
+    // todo 5.) If one of the following is click create a alert to simulate the next phase of the game
+    if (nameOptionElements.length > 0 && businessNameInput) {
+        nameOptionElements.forEach(nameOption => {
+            nameOption.style.cursor = 'pointer';
+            nameOption.addEventListener('click', function() {
+                alert("Business name selected, moving to the next phase...");
+            });
+        });
+        
+        // todo 6.) if the input field is not empty, goal-icons div should not be clickable (disabled)
+        businessNameInput.addEventListener('input', function() {
+            const inputValue = this.value.trim();
+            
+            if (inputValue !== '') {
+                nameOptionElements.forEach(nameOption => {
+                    nameOption.style.pointerEvents = 'none';
+                    nameOption.style.opacity = '0.5';
+                });
+            } else {
+                nameOptionElements.forEach(nameOption => {
+                    nameOption.style.pointerEvents = 'auto';
+                    nameOption.style.opacity = '1';
+                });
+            }
+        });
+        
+        businessNameInput.addEventListener('keypress', function(event) {
+            if (event.key === 'Enter' && this.value.trim() !== '') {
+                alert("Custom business name '" + this.value + "' selected, moving to the next phase...");
+            }
+        });
+    }
+});
